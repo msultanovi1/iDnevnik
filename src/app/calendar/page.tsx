bc73a22d2c0b133b33d4c1b6adc07415a06087d5
nsokolovic7 <nikola.sokolovic@chili-chips.xyz>
2025-08-04 15:17:19 +0200

    First commit

---
+++ src/app/calendar/page.tsx
@@ -0,0 +1,13 @@
+// HelloPage.tsx
+import React from 'react';
+import './calendar.css'
+const HelloPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Welcome to the HelloPage in TypeScript (TSX).</p>
+    </div>
+  );
+};
+
+export default HelloPage;
